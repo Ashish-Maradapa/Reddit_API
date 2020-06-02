eeef78cebb6d68bfe1b644942240824c6eba1d6b
Ashish-Maradapa <ashish.maradapa@gmail.com>
2020-06-01 22:23:30 -0400

    scrapper completed

---
+++ online scrapper/scrapper.py
@@ -0,0 +1,34 @@
+#! python3
+
+import pandas as pd
+import datetime as dt
+import authorization
+
+reddit = authorization.authorizereddit()
+subreddit = reddit.subreddit('Nootropics')
+
+top_subreddit = subreddit.top(limit=500)
+
+topics_dict = {"title": [], \
+               "score": [], \
+               "id": [], "url": [], \
+               "comms_num": [], \
+               "created": [], \
+               "body": []}
+for submission in top_subreddit:
+    topics_dict["title"].append(submission.title)
+    topics_dict["score"].append(submission.score)
+    topics_dict["id"].append(submission.id)
+    topics_dict["url"].append(submission.url)
+    topics_dict["comms_num"].append(submission.num_comments)
+    topics_dict["created"].append(submission.created)
+    topics_dict["body"].append(submission.selftext)
+
+topics_data = pd.DataFrame(topics_dict)
+
+def get_date(created):
+    return dt.datetime.fromtimestamp(created)
+_timestamp = topics_data["created"].apply(get_date)
+topics_data = topics_data.assign(timestamp = _timestamp)
+
+topics_data.to_csv('FILENAME.csv', index=False)
